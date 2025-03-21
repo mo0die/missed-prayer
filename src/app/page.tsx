296243df06ade7cd3b93829dd96d95cadaf098ce
moodie <ahmad.develop@proton.me>
2025-03-21 00:06:54 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,12 @@ export default async function Home() {
   return (
     <HydrateClient>
       <div className="flex flex-col items-center justify-center">
+        <p className="sm:text-md text-center text-2xl font-bold">
+          This tool assumes you have not prayed, and should be used as a guide.
+          please do not use this as a tool to judge yourself or others. the tool
+          takes the birth date and subtracts 7 years from the current date. then
+          it calculates the missed prayers,fasts based on the number of days
+        </p>
         <YearsForm />
       </div>
     </HydrateClient>
